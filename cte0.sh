c4449467827838b30c3e13c847a6c52a7e74267c
Dario Kampkaspar <dario.kampkaspar@oeaw.ac.at>
2020-09-21 11:22:55 +0200

    translate comments to English

--- cte0.sh
+++ cte0.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-# grobes Skelett
+# general outline to create a wellformed XML
 perl -p0e "s@^[^\{]*(.*)@<cte>\1@s" "$1" > cte.xml
 perl -i -p0e 's@[^\}]*$@</cte>@s' cte.xml
 
-# problematische Zeichen ersetzen
+# replace reserved and problematic characters
 perl -i -p0e "s@\x{02}@_@g" cte.xml
 perl -i -p0e "s@&@&amp;@g" cte.xml
 
 # Replace special character combinations
 perl -i -p0e "s@\^-@@g" cte.xml
 
-# Teile im Prolog, die interessant sein könnten
+# Clean up prologue
 perl -i -p0e "s@\x{5c}TemplateSpacing@\n\x{5c}TemplateSpacing@g" cte.xml
 perl -i -p0e "s@\x{5c}User(\d+)@\n\x{5c}User\1@g" cte.xml
 perl -i -p0e "s@\x{5c}FontUser(\d+)@\n\x{5c}FontUser\1@g" cte.xml
 perl -i -p0e 's@\x{5c}User(\d+):([^,]+),(\d+)=(.+)@<pdef lfd="User\1" name="\2" n="\3" def="\4" />@g' cte.xml
 perl -i -p0e 's@\x{5c}FontUser(\d+):([^,]+),(\d+)=(.+)@<fdef lfd="FontUser\1" name="\2" n="\3" def="\4" />@g' cte.xml
 
-# Hauptgliederung
+# main outline
 perl -i -p0e "s@{(Description|Text|Notes\d|Apparatus\d|Fonts|Format|HeaderFooter)\x{5c}@<\1>@g" cte.xml
 perl -i -p0e "s@\\\\(Description|Text|Notes\d|Apparatus\d|Fonts|Format|HeaderFooter)\}@</\1>@g" cte.xml
 
-# Leerzeichenelement
+# we must not create elements without a name
 perl -i -p0e "s@{ @<Z>@g" cte.xml
 perl -i -p0e "s@\x{5c} \}@</Z>@g" cte.xml
 
-# Absatzmarker
+# paragraph marker
 perl -i -p0e 's@{P\x{5c}([^\x{5C}]*)\x{5C}P}@<P vals="\1" />@g' cte.xml
 
-# normale Elemente
+# other elements
 perl -i -p0e 's@{(.)([^\x{5C}]*)\x{5C}@<\1 vals="\2">@g' cte.xml
 perl -i -p0e "s@\x{5c}(.)\}@</\1>@g" cte.xml
